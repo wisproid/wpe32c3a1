94ffd277802dff1157d53c3ddc57b34892a88f96
Heri Cahyono <heri@hericah.my.id>
2024-09-11 07:00:19 +0700

    enable watchdog

--- src/main.rs
+++ src/main.rs
@@ -27,14 +27,23 @@ async fn main(spawner: Spawner) {
 
     let clocks = ClockControl::max(system.clock_control).freeze();
     let timg0 = TimerGroup::new_async(peripherals.TIMG0, &clocks);
-    embassy::init(&clocks, timg0);
+    let timg1 = TimerGroup::new_async(peripherals.TIMG1, &clocks);
 
+    // Enable the RWDT watchdog timer:
+    let mut wdt0 = timg1.wdt;
+    wdt0.enable();
+    wdt0.set_timeout(2u64.secs());
+    log::info!("Watchdog enabled!");
+
+    // Enable embassy
+    embassy::init(&clocks, timg0);
     esp_println::logger::init_logger_from_env();
 
     spawner.spawn(run()).ok();
 
     loop {
         log::info!("Hello from Main");
+        wdt0.feed();
         Timer::after(Duration::from_millis(1_000)).await;
     }
 }
