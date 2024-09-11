a6d2c386e4f16effa1043f848fbf69982a69595e
Heri Cahyono <heri@hericah.my.id>
2024-09-11 10:22:22 +0700

    done upgraded to esphal20

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+//! embassy hello world
+//!
+//! This is an example of running the embassy executor with multiple tasks
+//! concurrently.
+
+//% CHIPS: esp32 esp32c2 esp32c3 esp32c6 esp32h2 esp32s2 esp32s3
+//% FEATURES: embassy esp-hal-embassy/integrated-timers
+
 #![no_std]
 #![no_main]
 
@@ -6,44 +14,35 @@ use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
 use esp_hal::{
     clock::ClockControl,
-    embassy::{self},
     peripherals::Peripherals,
-    prelude::*,
-    timer::TimerGroup,
+    system::SystemControl,
+    timer::timg::TimerGroup,
 };
 
 #[embassy_executor::task]
 async fn run() {
     loop {
-        log::info!("Hello from an embassy thread");
+        esp_println::println!("Hello world from embassy using esp-hal-async!");
         Timer::after(Duration::from_millis(1_000)).await;
     }
 }
 
-#[main]
+#[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
-    let peripherals = Peripherals::take();
-    let system = peripherals.SYSTEM.split();
-
-    let clocks = ClockControl::max(system.clock_control).freeze();
-    let timg0 = TimerGroup::new_async(peripherals.TIMG0, &clocks);
-    let timg1 = TimerGroup::new_async(peripherals.TIMG1, &clocks);
-
-    // Enable the RWDT watchdog timer:
-    let mut wdt0 = timg1.wdt;
-    wdt0.enable();
-    wdt0.set_timeout(2u64.secs());
-    log::info!("Watchdog enabled!");
-
-    // Enable embassy
-    embassy::init(&clocks, timg0);
     esp_println::logger::init_logger_from_env();
+
+    esp_println::println!("Init!");
+    let peripherals = Peripherals::take();
+    let system = SystemControl::new(peripherals.SYSTEM);
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    let timg0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    esp_hal_embassy::init(&clocks, timg0.timer0);
 
     spawner.spawn(run()).ok();
 
     loop {
-        log::info!("Hello from Main");
-        wdt0.feed();
-        Timer::after(Duration::from_millis(1_000)).await;
+        esp_println::println!("Bing!");
+        Timer::after(Duration::from_millis(5_000)).await;
     }
 }
